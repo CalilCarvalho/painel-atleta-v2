95aa8535cec1b9ca2feb3a8fbb1beebf12002b0e
CalilCarvalho <calilcarvalho@outlook.com>
2025-08-22 15:12:35 -0300

    teste

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('alpine:init', () => {
-    
-    Alpine.data('testComponent', () => ({
-        
-        message: 'Funcionou! O Alpine.js está conectado corretamente ao script.'
-        
-    }));
+// A inicialização do Petite-Vue é um pouco diferente
 
-});
+const testComponent = {
+    // Nossas variáveis (propriedades)
+    message: 'Funcionou! O Petite-Vue está conectado corretamente.'
+};
+
+// 'Montamos' nosso componente na página
+PetiteVue.createApp(testComponent).mount();
